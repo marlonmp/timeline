f809a02ba8e82168ea9c81e916e6bf48f2ee91d2
marlonmp <marlonmp214@protonmail.com>
2023-01-26 00:23:23 -0500

    status models updated and task and taskAction models created

--- backend/apps/project/models.py
+++ backend/apps/project/models.py
@@ -10,18 +10,20 @@ class Project(models.Model):
     and it groups all project task and schedules
     """
 
-    STATUS_ACTIVE = 'AC'
-    STATUS_PAUSED = 'PS'
-    STATUS_REVIEW = 'RV'
+    STATUS_PENDING = 'PN'
+    STATUS_DEVELOP = 'DV'
     STATUS_FINISHED = 'FN'
-    STATUS_PRODUCTION = 'PR'
+    STATUS_REVIEW = 'RV'
     STATUS_DELETED = 'DL'
 
     CHOICE_STATUS = (
-        (STATUS_ACTIVE, 'Active'),
-        (STATUS_PAUSED, 'Paused'),
+        (STATUS_PENDING, 'Pending'),
+        (STATUS_DEVELOP, 'In develop'),
+        (STATUS_FINISHED, 'Finished'),
+        (STATUS_REVIEW, 'Under review'),
         (STATUS_DELETED, 'Deleted'),
     )
+
 
     uuid = models.UUIDField(unique=True, editable=False, default=uuid4)
 
@@ -33,7 +35,7 @@ class Project(models.Model):
 
     members = models.ManyToManyField('user.User', related_name='projects', blank=True)
 
-    status = models.CharField(max_length=2, choices=CHOICE_STATUS, default=STATUS_ACTIVE)
+    status = models.CharField(max_length=2, choices=CHOICE_STATUS, default=STATUS_PENDING)
 
     created_at = models.DateTimeField(editable=False, auto_now_add=True)
 
@@ -60,16 +62,16 @@ class Schedule(models.Model):
     This model represents a grpup of tasks
     """
 
-    STATUS_ACTIVE = 'AC'
+    STATUS_PENDING = 'PN'
     STATUS_IN_PROGRESS = 'PG'
-    STATUS_CLOSED = 'CL'
+    STATUS_SOLVED = 'SV'
     STATUS_PAUSED = 'PS'
     STATUS_DELETED = 'DL'
 
     CHOICE_STATUS = (
-        (STATUS_ACTIVE, 'Active'),
+        (STATUS_PENDING, 'Pending'),
         (STATUS_IN_PROGRESS, 'In progress'),
-        (STATUS_CLOSED, 'Closed'),
+        (STATUS_SOLVED, 'Solved'),
         (STATUS_PAUSED, 'Paused'),
         (STATUS_DELETED, 'Deleted'),
     )
@@ -84,39 +86,66 @@ class Schedule(models.Model):
 
     description = models.CharField(max_length=240, blank=True)
 
-    status = models.CharField(max_length=2, choices=CHOICE_STATUS, default=STATUS_ACTIVE)
+    status = models.CharField(max_length=2, choices=CHOICE_STATUS, default=STATUS_PENDING)
 
     created_at = models.DateTimeField(editable=False, auto_now_add=True)
 
 
-class Task(models.Model):
+class AbstractTask(models.Model):
 
-    STATUS_ACTIVE = 'AC'
+    STATUS_PENDING = 'PN'
     STATUS_IN_PROGRESS = 'PG'
     STATUS_SOLVED = 'SV'
-    STATUS_CLOSED = 'CL'
     STATUS_PAUSED = 'PS'
     STATUS_DELETED = 'DL'
 
     CHOICE_STATUS = (
-        (STATUS_ACTIVE, 'Active'),
+        (STATUS_PENDING, 'Pending'),
         (STATUS_IN_PROGRESS, 'In progress'),
         (STATUS_SOLVED, 'Solved'),
-        (STATUS_CLOSED, 'Closed'),
         (STATUS_PAUSED, 'Paused'),
         (STATUS_DELETED, 'Deleted'),
     )
 
     uuid = models.UUIDField(unique=True, editable=False, default=uuid4)
 
-    schedule = models.ForeignKey('project.Schedule', on_delete=models.PROTECT, related_name='tasks')
+    description = models.CharField(max_length=240, blank=True)
+
+    const_time = models.DurationField(null=True, blank=True)
+
+    status = models.CharField(max_length=2, choices=CHOICE_STATUS, default=STATUS_PENDING, null=True)
+
+    created_at = models.DateTimeField(editable=False, auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+class Task(AbstractTask):
 
     owner = models.ForeignKey('user.User', on_delete=models.PROTECT, related_name='tasks', null=True)
 
-    description = models.CharField(max_length=240, blank=True)
+    schedule = models.ForeignKey('project.Schedule', on_delete=models.PROTECT, related_name='tasks')
 
-    const_time = models.DurationField(null=True)
+    def save(self, *args, **kwargs):
 
-    status = models.CharField(max_length=2, choices=CHOICE_STATUS, default=STATUS_ACTIVE)
+        attrs = { 'dispatcher': '' }
+        fields = ['owner', 'description', 'const_time', 'status']
 
-    created_at = models.DateTimeField(editable=False, auto_now_add=True)
+        for field in fields:
+            value = getattr(self, field)
+            if value:
+                attrs[field] = value
+
+        TaskAction(**attrs).save()
+
+        return super().save(*args, **kwargs)
+
+
+class TaskAction(AbstractTask):
+
+    owner = models.ForeignKey('user.User', on_delete=models.PROTECT, related_name='assigned_tasks', null=True)
+
+    dispatcher = models.ForeignKey('user.User', on_delete=models.PROTECT, related_name='task_actions')
+
+    task = models.ForeignKey('project.Task', on_delete=models.PROTECT, related_name='task_actions')
